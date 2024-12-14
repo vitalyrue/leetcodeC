cc59c03bf4bd15e807bc5703ccde1aaab7d35f73
vitalyrue <vitalyr@gmail.com>
2024-12-14 20:20:23 +0200

    Update longest_substring_without_repeat.cc
    shorter ver

--- 0003_longest_substring_without_repeat/longest_substring_without_repeat.cc
+++ 0003_longest_substring_without_repeat/longest_substring_without_repeat.cc
@@ -4,7 +4,7 @@ using namespace std;
 
 class Solution {
 public:
-    int lengthOfLongestSubstring(string s) {
+    /*int lengthOfLongestSubstring(string s) {
         vector<int> count(256);
         int len = 0;
         int i, j;
@@ -18,5 +18,25 @@ public:
         }
 
         return i - j > len ? i - j : len;
+    }*/
+
+    int lengthOfLongestSubstring(char *s) {
+        vector<int> count(256);  // Frequency of characters
+        int len = 0;           // Longest substring length
+        int i = 0, j = 0;      // Sliding window pointers
+    
+        for (i = 0; s[i] != '\0'; i++) {
+            count[s[i]]++;  // Add s[i] to the window
+    
+            // If a duplicate is introduced, shrink the window
+            if (count[s[i]] > 1) {
+                count[s[j++]]--;  // Remove the character at s[j] and move j forward
+            }
+            // Update the maximum length of the valid window
+            len = (i - j + 1 > len) ? (i - j + 1) : len;
+        }
+    
+        return len;
     }
+
 };
