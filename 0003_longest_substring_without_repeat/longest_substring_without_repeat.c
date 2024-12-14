597ada18a844067b13ea0aa804bb12c9d14d82f5
vitalyrue <vitalyr@gmail.com>
2024-12-14 20:17:46 +0200

    Update longest_substring_without_repeat.c
    simplified algorithm

--- 0003_longest_substring_without_repeat/longest_substring_without_repeat.c
+++ 0003_longest_substring_without_repeat/longest_substring_without_repeat.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 
-
+/* algorithm: very intrasparent. 
 int lengthOfLongestSubstring(char *s)
 {
     int count[256] = {0};
@@ -10,15 +10,37 @@ int lengthOfLongestSubstring(char *s)
     int i, j;
 
     for (i = 0, j = 0; s[i] != '\0'; i++) {
-        count[s[i]]++;
-        while (count[s[i]] > 1) {
-            len = i - j > len ? i - j : len;
+        count[s[i]]++; // increase this first char counter, expanding the string
+        while (count[s[i]] > 1) {                     // if last char of window becomes duplicate after the move
+            len = (i - j > len) ? (i - j) : len;      // 
             count[s[j++]] -= 1;
         }
     }
 
     return i - j > len ? i - j : len;
+} */
+// simplified version
+int lengthOfLongestSubstring(char *s) {
+    int count[256] = {0};  // Frequency of characters
+    int len = 0;           // Longest substring length
+    int i = 0, j = 0;      // Sliding window pointers
+
+    for (i = 0; s[i] != '\0'; i++) {
+        count[s[i]]++;  // Add s[i] to the window
+
+        // If a duplicate is introduced, shrink the window
+        if (count[s[i]] > 1) {
+            count[s[j++]]--;  // Remove the character at s[j] and move j forward
+        }
+
+        // Update the maximum length of the valid window
+        len = (i - j + 1 > len) ? (i - j + 1) : len;
+    }
+
+    return len;
 }
+
+
 
 int main(int argc, char **argv)
 {
